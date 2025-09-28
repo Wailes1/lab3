e0dd3492c36a685353131e0291117fcc0ef9b12b
Wailes1 <koulzx@gmail.com>
2025-09-28 17:36:23 +0300

    2

--- 3.py
+++ 3.py
@@ -6,3 +6,8 @@ def task1():
     """1. List comprehension (простое преобразование)"""
     squares = [x**2 for x in range(1, 11)]
     print("Квадраты чисел от 1 до 10:", squares)
+    
+def task2():
+    """2. List comprehension (фильтрация)"""
+    evens = [x for x in range(1, 20) if x % 2 == 0]
+    print("Четные числа от 1 до 20:", evens)
