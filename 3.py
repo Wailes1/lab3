498bcf92645ee2b60a66c503c9707d0c3ec6ae20
Дмитрий Лютиков <koulzx@gmail.com>
2025-09-28 17:16:30 +0300

    1

---
+++ 3.py
@@ -0,0 +1,9 @@
+from datetime import datetime, date
+from decimal import Decimal
+from fractions import Fraction
+
+def task1():
+    """1. List comprehension (простое преобразование)"""
+    squares = [x**2 for x in range(1, 11)]
+    print("Квадраты чисел от 1 до 10:", squares)
+
